e88af9cf4fdc845bf0ced9a2b64e72ec90e5c0fd
ChloeArd <chloe.ardoise@gmail.com>
2020-12-22 11:15:27 +0100

    Fenetre modale

--- app.js
+++ app.js
@@ -17,10 +17,25 @@ function Modal(titre, texte, label){
         let label = document.getElementById("label1");
         label.innerHTML = this.label;
     }
+
+    this.ajoutModal = function () {
+        let window = document.getElementById("window");
+        window.style.display = "block";
+    }
+    
+    this.close = function (id){
+        document.getElementById(id).addEventListener("click", function (){
+            let window = document.getElementById("window");
+            window.style.display = "none";
+        })
+    }
 }
 
 let modal1 = new Modal("Modal Window", "Are you sure want to download this modal box?<br>\n" + "Click 'OK' to start downloading.", "Agree ou terms & conditions!")
 modal1.ajoutLabel();
 modal1.ajoutParagraph();
 modal1.ajoutTitre();
-
+modal1.ajoutModal();
+modal1.close("close");
+new modal1.close("ok");
+new modal1.close("cancel");
